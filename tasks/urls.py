f0689b0752c41f8784ed85970fd5efafa7a46b6e
Tetiana Sobko <sobkotiana@gmail.com>
2025-03-19 01:40:53 +0200

    Add index view

--- tasks/urls.py
+++ tasks/urls.py
@@ -5,3 +5,5 @@ from tasks.views import index
 urlpatterns = [
     path("", index, name="index"),
 ]
+
+app_name = "tasks"
--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+from tasks.models import Task, Worker, TaskType, Position
+
+
+def index(request):
+    context = {
+        "num_tasks": Task.objects.count(),
+        "num_workers": Worker.objects.count(),
+        "num_task_types": TaskType.objects.count(),
+        "num_positions": Position.objects.count(),
+    }
+    return render(request, "tasks/index.html", context=context)
