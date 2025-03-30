709ce27a64015f6e5771301a8953af8e1bbc2e60
Tetiana Sobko <sobkotiana@gmail.com>
2025-03-30 17:53:06 +0300

    Update requirements and settings

--- task_manager/settings/prod.py
+++ task_manager/settings/prod.py
@@ -1,10 +1,12 @@
-import os
-
 from .base import *
 
 DEBUG = False
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
+ALLOWED_HOSTS = []
+
+RENDER_EXTERNAL_HOSTNAME = os.environ.get("RENDER_EXTERNAL_HOSTNAME")
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
@@ -17,9 +19,10 @@ DATABASES = {
         "PASSWORD": os.environ["POSTGRES_PASSWORD"],
         "HOST": os.environ["POSTGRES_HOST"],
         "PORT": int(os.environ["POSTGRES_DB_PORT"]),
+        "OPTIONS": {
+            "sslmode": "require",
+        },
     }
 }
 
-SECURE_SSL_REDIRECT=True
-SESSION_COOKIE_SECURE=True
-CSRF_COOKIE_SECURE=True
+ROOT_URLCONF = 'task_manager.urls'
